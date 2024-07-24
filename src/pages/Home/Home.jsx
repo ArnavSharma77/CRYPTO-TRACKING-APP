a310d836a1f4514eaae689d8c4158234e4169b00
ArnavSharma77 <115538878+ArnavSharma77@users.noreply.github.com>
2024-07-24 20:16:53 +0530

    crypto tracker

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,82 @@
+import React, { useContext, useEffect, useState } from 'react'
+import './Home.css'
+import { CoinContext } from '../../context/CoinContext'
+import {Link} from 'react-router-dom'
+
+export default function Home () {
+
+const {allCoin , currency} = useContext(CoinContext);
+const [displayCoin,setDisplayCoin] = useState([]);
+const [input,setInput] = useState("");
+
+const inputHandler = (event) => {
+    setInput(event.target.value);
+    //we want page to show all 10 default coins again if nothing is written:-
+    if (event.target.value === "") {
+        setDisplayCoin(allCoin);
+    }
+}
+
+const searchHandler = async (event) => {
+//prevents site from reloading when we write something and enter:-
+    event.preventDefault();
+    const coins = await allCoin.filter((item)=>{
+        return item.name.toLowerCase().includes(input.toLowerCase())
+    })
+    //types - bit , will show bitcoin (bitcoin includes bit and all coins who show bit)
+    setDisplayCoin(coins);
+}
+
+useEffect(()=>{
+    setDisplayCoin(allCoin);
+},[allCoin])
+
+  return (
+    <div className = 'home'>
+      <div className = "hero">
+        <h1>Largest <br/> Crypto Marketplace</h1>
+        <p>Welcome to the world's largest cryptocurrency
+        marketplace. Sign up to explore more about cryptos.</p>
+        <form onSubmit = {searchHandler}>
+
+
+            <input onChange = {inputHandler} list = 'coinlist' value = {input} type = "text" placeholder = 'Search crypto...' required/>
+            <button type = "submit">Search</button>
+
+            <datalist id = 'coinlist'>
+                {allCoin.map((item,index)=>(
+                    <option key = {index} value = {item.name} />
+                ))}
+            </datalist>
+
+        </form>
+      </div>
+      <div className = "crypto-table">
+        <div className = "table-layout">
+            <p>#</p>
+            <p>Coins</p>
+            <p>Price</p>
+            <p style = {{textAlign : "center"}}>24 H Change</p>
+            <p className = "market-cap">Market Cap</p>
+        </div> 
+        {
+            displayCoin.slice(0,10).map((item , index)=>(
+                <Link to = {`/coin/${item.id}`} className="table-layout" key = {index}>
+                    <p>{item.market_cap_rank}</p>
+                <div>
+                    <img src = {item.image} alt = "" />
+                    <p>{item.name + "-" + item.symbol}</p>
+                </div>
+                <p>{currency.symbol} {item.current_price.toLocaleString()}</p>
+                {/* dynamic class:- */}
+                <p className = {item.price_change_percentage_24h>0 ? "green" : "red"}>{Math.floor(item.price_change_percentage_24h*100)/100}</p>
+                <p className = 'market-cap'>{currency.symbol} {item.market_cap.toLocaleString()}</p>
+                </Link>
+            ))
+        }
+      </div>
+    </div>
+  )
+}
+
+
